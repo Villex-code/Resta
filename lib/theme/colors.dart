4c6ebd533ebd6e58e92f0bddbacf8a26d2332c7b
Villex <videlik@gmail.com>
2023-05-28 00:55:26 +0300

    all logins added

---
+++ lib/theme/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final white = const Color(0xffe9e4e1);
+  final black = const Color(0xff252525);
+  final green = const Color(0xff1db954);
+  final grey = const Color(0xffa5a6ac);
+  final blue = const Color(0xff4462e6);
+  final pink = const Color(0xffe7119c);
+
+  const AppColors();
+}
