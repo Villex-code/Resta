301e357f93fb61eaceba57f51745947c233794c5
Villex <videlik@gmail.com>
2023-05-29 14:49:50 +0300

    fixed

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,9 +1,6 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:test_app/menu.dart';
-import 'package:test_app/main.dart';
-import 'package:test_app/addtable.dart';
+import 'package:my_app/addtable.dart';
 
 class Notifications extends StatelessWidget {
   const Notifications({super.key});
@@ -14,81 +11,76 @@ class Notifications extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/rese_back.jpg'),fit: BoxFit.cover)),
+              image: AssetImage('assets/rese_back.jpg'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.black54,
           leading: IconButton(
-            onPressed: (){},
+            onPressed: () {},
             icon: Icon(Icons.arrow_back),
           ),
-          title: Row(
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/company.png'),
-                ),
-                SizedBox(width: 20.0,),
-                Text('Company name'),
-              ]
-          ),
+          title: Row(children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/company.png'),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Text('Company name'),
+          ]),
         ),
-        body: Stack(
-            children:<Widget> [
-              SingleChildScrollView(
-                child:Column(
-                    children: [
-                      Card(
-                        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              CircleAvatar(
-                                backgroundImage: AssetImage('assets/company.png'),
-                              ),
-                              Text('Rserve Info'),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => const AddTable()),
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.green, // Background color
-                                ),
-                                icon: Icon(
-                                  Icons.check,
-                                  size: 24.0,
-                                ),
-                                label: Text("Accept"),
-                              ),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.redAccent, // Background color
-                                ),
-                                icon: Icon(
-                                  Icons.cancel,
-                                  size: 24.0,
-                                ),
-                                label: Text("Refuse"),
-                              ),
-                            ],
-                          ),
-                        ),
+        body: Stack(children: <Widget>[
+          SingleChildScrollView(
+            child: Column(children: [
+              Card(
+                margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/company.png'),
                       ),
-                    ]
+                      Text('Rserve Info'),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const AddTable()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green, // Background color
+                        ),
+                        icon: Icon(
+                          Icons.check,
+                          size: 24.0,
+                        ),
+                        label: Text("Accept"),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.redAccent, // Background color
+                        ),
+                        icon: Icon(
+                          Icons.cancel,
+                          size: 24.0,
+                        ),
+                        label: Text("Refuse"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ]),
+          ),
+        ]),
       ),
     );
-
   }
 }
-
--- lib/table_view.dart
+++ lib/table_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/addtable.dart';
-import 'package:test_app/menu.dart';
-import 'package:test_app/main.dart';
-
+import 'package:my_app/addtable.dart';
 
 class TableView extends StatelessWidget {
   const TableView({super.key});
@@ -12,7 +9,7 @@ class TableView extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/table1.jpg'),fit: BoxFit.cover)),
+              image: AssetImage('assets/table1.jpg'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
@@ -20,40 +17,43 @@ class TableView extends StatelessWidget {
           backgroundColor: Colors.black54,
           leading: IconButton(
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SeeStoreView()),
-              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const SeeStoreView()),
+              // );
             },
             icon: Icon(Icons.arrow_back),
           ),
-          title:Row(
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/company.png'),
-                ),
-                SizedBox(width: 20.0,),
-                Text('Company name'),
-              ]),
-        ),
-        body:Container(
-          child: SingleChildScrollView(
-            child: Column(
-                children:[
-                  Container(
-                    height: 100,
-                    child: Center(
-                        child: Text("Table View", style: TextStyle(fontFamily: 'OoohBaby',fontSize: 50,color:Colors.white))
-                    ),
-                  ),
-                  SizedBox(height: 30.0,),
-                  Image.asset('assets/table_view.jpg'),
-                ]
+          title: Row(children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/company.png'),
             ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Text('Company name'),
+          ]),
+        ),
+        body: Container(
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Container(
+                height: 100,
+                child: Center(
+                    child: Text("Table View",
+                        style: TextStyle(
+                            fontFamily: 'OoohBaby',
+                            fontSize: 50,
+                            color: Colors.white))),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Image.asset('assets/table_view.jpg'),
+            ]),
           ),
         ),
-        floatingActionButtonLocation:
-        FloatingActionButtonLocation.centerFloat,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: Container(
           height: 50,
           margin: const EdgeInsets.all(10),
@@ -78,4 +78,3 @@ class TableView extends StatelessWidget {
     );
   }
 }
-
